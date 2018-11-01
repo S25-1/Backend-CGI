94ce3de29c01a043ec36b17ca09025ecf844566c
Wang,Mike M <mike.wang@student.fontys.nl>
2018-11-01 14:11:43 +0100

    updated

--- cgiAPI/Controllers/VacancyController.cs
+++ cgiAPI/Controllers/VacancyController.cs
@@ -1,18 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace cgiAPI.Controllers
 {
-    public class VacancyController : Controller
+    public class VacancyController : ApiController
     {
-        // GET: Vacancy
-        public ActionResult Index()
+        // GET api/values
+        public IEnumerable<string> Get()
         {
 
-            return View();
+
+            return new string[] { "value1", "value2" };
+
+        }
+
+        // GET api/values/5
+        public int Get(int value)
+        {
+            return value;
+        }
+
+        public void CreateVacancy(int value)
+        {
+            //return value;
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            Employer userTest = new Employer(1, 1, "name", "test@hotmail.com", "password", 1, new List<SkillType>() { new SkillType(1, "programmer") });
+            JobOffer jobOffer = new JobOffer(userTest, "We zoeken naar programmeur", 1, new List<SkillType>() { new SkillType(1, "programmer") }, "Een geervaarde programmeur", new DateTime(2018, 11, 1), new DateTime(2018, 11, 11), 5);
+            JobOffer.AddJobOffer(jobOffer);
+        }
+
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
         }
     }
 }
