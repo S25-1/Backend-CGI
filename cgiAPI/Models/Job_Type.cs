9f36750f30652a1dbe7f3e1ab611f85983e90248
Wang,Mike M <mike.wang@student.fontys.nl>
2018-11-15 10:40:19 +0100

    save point

--- cgiAPI/Models/Job_Type.cs
+++ cgiAPI/Models/Job_Type.cs
@@ -7,8 +7,13 @@ namespace cgiAPI.Models
 {
     public class Job_Type
     {
+        public int Job_typeID { get; set; }
+        public string Job_name { get; set; }
 
-        public int Job_TypeID { get; set; }
-        public string Job_naam { get; set; }
+        public Job_Type(int job_typeID, string job_name)
+        {
+            Job_typeID = job_typeID;
+            Job_name = job_name;
+        }
     }
 }
